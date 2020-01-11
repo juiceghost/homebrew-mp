dea622dda972d6875873f2a4640f600411d6df2a
juiceghost <jg@renegade.se>
2020-01-11 23:58:59 +0100

    Formula tweak

--- HomebrewFormula/mp.rb
+++ HomebrewFormula/mp.rb
@@ -2,7 +2,7 @@ class Mp < Formula
   desc "Console-based editor by Ángel Ortega"
   homepage "https://triptico.com/software/mp.html"
   url "https://github.com/juiceghost/homebrew-mp/archive/v3.2.14.tar.gz"
-  sha256 "52883d4dcf69429cc088efc767a9d425af3e739f1e09d54fbee94190f7f80d87"
+  sha256 ""
 
   def install
     system "make", "mp"
