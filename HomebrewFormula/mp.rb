9c1a35af8671508fd54407ca801e8661afe5bcb3
juiceghost <jg@renegade.se>
2020-01-12 00:04:33 +0100

    Added checksum

--- HomebrewFormula/mp.rb
+++ HomebrewFormula/mp.rb
@@ -2,7 +2,7 @@ class Mp < Formula
   desc "Console-based editor by Ángel Ortega"
   homepage "https://triptico.com/software/mp.html"
   url "https://github.com/juiceghost/homebrew-mp/archive/v3.2.14a.tar.gz"
-  sha256 ""
+  sha256 "b4f0bde929526999eca8d161b5c345003b1c383104142e2f6401ab67c83d4371"
 
   def install
     system "make", "mp"
